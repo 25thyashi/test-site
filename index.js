0b1ef57bd58e40b1ab58badeac42e1d4da42875e
25thyashi <yashi.gupta@ceew.in>
2026-01-21 21:03:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.add("animation-ready");
+function toggle() {
+  document.body.classList.add('animation-ready');
+  document.body.classList.toggle('dark');
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode === 32) {
+    toggle();
+  }
 });
 
+document.addEventListener('click', function() {
+  toggle();
+});
